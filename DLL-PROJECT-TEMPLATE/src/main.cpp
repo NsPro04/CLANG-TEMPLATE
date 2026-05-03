3d8a865554a7b7ab55899096fcad168c0d98e1f6
NsPro04 <nikitasolovev697@gmail.com>
2026-05-03 18:30:15 +0600

    src files have been added + fix conan cmaketoolchain

---
+++ DLL-PROJECT-TEMPLATE/src/main.cpp
@@ -0,0 +1,21 @@
+﻿/*
+/// .h:
+#ifdef XXXX_EXPORTS
+#  define XXXX_API __declspec(dllexport)
+#else // !XXXX_EXPORT
+#  define XXXX_API __declspec(dllimport)
+#  ifndef XXXX_NO_AUTO_LINK
+#    pragma comment(lib, "xxxx.lib")
+#  endif // XXXX_NO_AUTO_LINK
+#endif // XXXX_EXPORT
+
+namespace xxxx {
+inline namespace v1 {
+XXXX_API ...;
+} // namespace v1
+} // namespace xxxx
+/// :.h
+
+#define XXXX_EXPORTS
+#include <xxxx/xxxx.h>
+*/
